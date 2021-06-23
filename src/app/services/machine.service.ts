2950f8c962325a101d4f627bd099ba33df4c3e4a
nouhamejri <nouha.mejri@esprit.tn>
2021-06-23 18:56:46 +0200

    fixCorsOrigin

---
+++ src/app/services/machine.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { MachineInput } from "../models/machineInput";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MachineService{
+    constructor(private _http: HttpClient){
+    }
+
+
+    getAllMachines(){
+        return this._http.get<any>(
+            `/api/listmachines` 
+        )
+    }
+    getMachineByIdentifier(identifier: string) {
+        return this._http.get<any>(
+          `/api/machine` + identifier
+        )
+      }
+      getMachineByFilters(filters: string) {
+        return this._http.get<any>(
+          `/api/machine?` + filters
+        )
+      }
+    createMachine(body: MachineInput) {
+        return this._http.post<any>(
+          `/api/addMachine`,
+          body
+        )
+      }
+    removeMachine(identifier: string) {
+        return this._http.post<any>(
+          `/api/removeMachine/` + identifier, {}
+        )
+      }
+
+}
