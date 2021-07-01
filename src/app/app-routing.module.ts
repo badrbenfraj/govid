7e173373c78796f15f56195833224ecaca81287f
Rhamza97 <roua.hamza@esprit.tn>
2021-07-01 23:01:30 +0100

    fix(Medecin): Add gestionMedecins

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { SignupComponent } from './core/auth/signup/signup.component';
 import {NotFoundComponent} from './core/not-found/not-found.component';
 import {HomeComponent} from './components/home/home.component';
 import {ForgotPasswordComponent} from './core/auth/forgot-password/forgot-password.component';
+import { ListMedecinsComponent } from './components/Medecins/list-medecins/list-medecins.component';
+import { AjouterMedecinComponent } from './components/Medecins/ajouter-medecin/ajouter-medecin.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: SignupComponent },
   { path: 'reset', component: ForgotPasswordComponent },
+  { path: 'ListMedecins', component: ListMedecinsComponent },
+  { path: 'AddMedecin', component: AjouterMedecinComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' },
 ];
---
+++ src/app/components/Medecins/ajouter-medecin/ajouter-medecin.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AjouterMedecinComponent } from './ajouter-medecin.component';
+
+describe('AjouterMedecinComponent', () => {
+  let component: AjouterMedecinComponent;
+  let fixture: ComponentFixture<AjouterMedecinComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AjouterMedecinComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AjouterMedecinComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
