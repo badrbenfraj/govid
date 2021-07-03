d64f5af76892a56672047b4d77a12d0a65f86f5c
Badredine Benfraj <badrbenfraj@mail.com>
2021-07-03 09:48:26 +0100

    feat(): add authentication

--- api/src/Controller/SecurityController.php
+++ api/src/Controller/SecurityController.php
@@ -4,34 +4,43 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use App\Security\LoginAuthenticator;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Context\Context;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class SecurityController extends AbstractFOSRestController
 {
-    public function __construct(UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder)
-    {
-        $this->userRepository = $userRepository;
-    }
+  /**
+   * @var UserRepository
+   */
+  private $userRepository;
+  /**
+   * @var UserPasswordEncoderInterface
+   */
+  private $passwordEncoder;
+  /**
+   * @var EntityManagerInterface
+   */
+  private $entityManager;
+
+  public function __construct(UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager)
+  {
+    $this->userRepository = $userRepository;
+    $this->passwordEncoder = $passwordEncoder;
+    $this->entityManager = $entityManager;
+  }
 
   /**
      * @Route("/login", name="app_login", methods={"POST"})
      */
-    public function login(AuthenticationUtils $authenticationUtils, Request $request)
+    public function login( Request $request)
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-      $email = $request->attributes->get('email');
-        return new JsonResponse($email);
 
     }
 
@@ -43,19 +52,48 @@ class SecurityController extends AbstractController
         throw new \Exception('Will be intercepted before getting here');
     }
 
-    /**
-     * @Route("/register", name="app_register", methods={"POST"})
-     */
-    public function register(Request $request)
-    {
-      $data = json_decode($request->getContent(), true);
-      $email = $data['email'];
-      $password = $data['password'];
-      $firstName = $data['firstName'];
-      $lastName = $data['lastName'];
 
-      $user = $this->userRepository->findOneBy(['email' => $email]);
-      return new JsonResponse($data);
+  /**
+   * @Route("/register", name="register")
+   * @param Request $request
+   * @return \FOS\RestBundle\View\View
+   */
+  public function register(Request $request)
+  {
+    $data = json_decode($request->getContent(), true);
+    $email = $data['email'];
+    $firstName = $data['firstName'];
+    $lastName = $data['lastName'];
+    $roles = $data['roles'];
+    $password = $data['password'];
 
+    $user = $this->userRepository->findOneBy([
+      'email' => $email,
+    ]);
+
+    if (!is_null($user)) {
+      return $this->view([
+        'message' => 'User already exists',
+        'code' => Response::HTTP_CONFLICT
+      ], Response::HTTP_CONFLICT);
     }
+
+    $user = new User();
+
+    $user->setEmail($email);
+    $user->setFirstName($firstName);
+    $user->setLastName($lastName);
+    $user->setRoles($roles);
+    $user->setPassword(
+      $this->passwordEncoder->encodePassword($user, $password)
+    );
+
+    $this->entityManager->persist($user);
+    $this->entityManager->flush();
+
+    return $this->view([
+        'message' => 'User Created Successfully',
+        'code' => Response::HTTP_CREATED
+      ], Response::HTTP_CREATED)->setContext((new Context())->setGroups(['public']));
+  }
 }
