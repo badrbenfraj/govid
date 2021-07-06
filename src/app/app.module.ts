f4e6a38101420d1667b61eac1165b1c1f058fd12
Badredine Benfraj <badrbenfraj@mail.com>
2021-07-07 00:26:44 +0100

    refactor(): improve styling and add modules to login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { FormsModule } from '@angular/forms'
 import { MatCardModule } from '@angular/material/card'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { LoginComponent } from '@auth/login/login.component'
-import { SignupComponent } from '@auth/signup/signup.component'
 import { NotFoundComponent } from '@core/not-found/not-found.component'
 import { HomeComponent } from '@components/home/home.component'
 import { ForgotPasswordComponent } from '@auth/forgot-password/forgot-password.component'
@@ -54,8 +52,6 @@ import {MatPaginatorModule} from '@angular/material/paginator'
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
     NotFoundComponent,
     HomeComponent,
     ForgotPasswordComponent,
